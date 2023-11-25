610fd130d37042ed2e33308ebbc6de4be18d0ad1
Mohit kachhaway <130671896+MohitKachhway@users.noreply.github.com>
2023-11-25 18:52:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // your code here
+let form = document.querySelector("form");
+// console.log(form)
+function fun(event){
+    event.preventDefault();
+    let name = document.getElementById("name").value;
+    let year = document.getElementById("year").value;
+    let heading =document.querySelector("h3");
+    if(name&&year){
+        heading.innerText= `https://localhost:8080/?name=${name}&year=${year}`
+    }else if(name){
+        heading.innerText= `https://localhost:8080/?name=${name}`
+    }else
+    heading.innerText= `https://localhost:8080/?year=${year}`
+}
+form.addEventListener("submit",fun);
